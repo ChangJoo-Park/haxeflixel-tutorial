fa84a8a721045ee3dfea7378e73c210453af2aa5
ChangJoo Park <pcjpcj2@gmail.com>
2019-08-03 22:16:41 +0900

    load game map from ogmoeditor

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,17 +1,41 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.tile.FlxTilemap;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
 import flixel.FlxState;
 
 class PlayState extends FlxState {
 	var _player:Player;
+	var _map:FlxOgmoLoader;
+	var _mWalls:FlxTilemap;
 
 	override public function create():Void {
 		super.create();
-		_player = new Player(20, 20);
+		_map = new FlxOgmoLoader(AssetPaths.room_001__oel);
+		_mWalls = _map.loadTilemap(AssetPaths.tiles__png, 16, 16, "walls");
+		_mWalls.follow();
+		_mWalls.setTileProperties(1, FlxObject.NONE);
+		_mWalls.setTileProperties(2, FlxObject.ANY);
+		add(_mWalls);
+
+		_player = new Player();
+		_map.loadEntities(placeEntities, "entities");
 		add(_player);
 	}
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
+		FlxG.collide(_player, _mWalls);
+	}
+
+	function placeEntities(entityName:String, entityData:Xml):Void {
+		var x:Int = Std.parseInt(entityData.get("x"));
+		var y:Int = Std.parseInt(entityData.get("y"));
+		if (entityName == "player") {
+			_player.x = x;
+			_player.y = y;
+		}
 	}
 }
