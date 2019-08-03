c1a38f34604718509763ab1515d15fd2fea6900d
ChangJoo Park <pcjpcj2@gmail.com>
2019-08-03 21:43:50 +0900

    Set Main state to MenuState

--- source/Main.hx
+++ source/Main.hx
@@ -1,12 +1,11 @@
 package;
 
-import haxe.iterators.StringIteratorUnicode;
 import flixel.FlxGame;
 import openfl.display.Sprite;
 
 class Main extends Sprite {
 	public function new() {
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+		addChild(new FlxGame(0, 0, MenuState));
 	}
 }
