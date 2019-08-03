09b2c5227cdec6877afc359535d06ec1c56fc32b
ChangJoo Park <pcjpcj2@gmail.com>
2019-08-03 19:42:36 +0900

    Add FlxText

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,13 +1,15 @@
 package;
 
+import flixel.text.FlxText;
 import flixel.FlxState;
 
 class PlayState extends FlxState {
 	override public function create():Void {
 		super.create();
-		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
+		var text = new FlxText(0, 0, 0, "Hello World", 64);
 		text.screenCenter();
 		add(text);
+		add(new FlxText(10, 10, 100, "Hello, World!"));
 	}
 
 	override public function update(elapsed:Float):Void {
