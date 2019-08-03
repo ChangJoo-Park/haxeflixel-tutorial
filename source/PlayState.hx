1c334a93c648b1c784bff656bc81a6779fb046ba
ChangJoo Park <pcjpcj2@gmail.com>
2019-08-03 21:43:32 +0900

    add player when created PlayState

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,11 +3,12 @@ package;
 import flixel.FlxState;
 
 class PlayState extends FlxState {
+	var _player:Player;
+
 	override public function create():Void {
 		super.create();
-		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
-		text.screenCenter();
-		add(text);
+		_player = new Player(20, 20);
+		add(_player);
 	}
 
 	override public function update(elapsed:Float):Void {
