011bd50ecca3b1841dc27d9e14da9680fbba4e31
ChangJoo Park <pcjpcj2@gmail.com>
2019-08-03 21:54:09 +0900

    Add animation when move

--- source/Player.hx
+++ source/Player.hx
@@ -1,3 +1,4 @@
+import flixel.FlxObject;
 import flixel.math.FlxPoint;
 import flixel.FlxG;
 import flixel.util.FlxColor;
@@ -14,7 +15,12 @@ class Player extends FlxSprite {
 	public function new(?X:Float = 0, ?Y:Float = 0) {
 		super(X, Y);
 
-		makeGraphic(16, 16, FlxColor.BLUE);
+		loadGraphic(AssetPaths.player__png, true, 16, 16);
+		setFacingFlip(FlxObject.LEFT, false, false);
+		setFacingFlip(FlxObject.RIGHT, true, false);
+		animation.add("lr", [3, 4, 3, 5], 6, false);
+		animation.add("u", [6, 7, 6, 8], 6, false);
+		animation.add("d", [0, 1, 0, 2], 6, false);
 		drag.x = drag.y = 1600;
 	}
 
@@ -54,6 +60,7 @@ class Player extends FlxSprite {
 			} else if (_right) {
 				mA += 45;
 			}
+			facing = FlxObject.UP;
 		} else if (_down) {
 			mA = 90;
 			if (_left) {
@@ -61,13 +68,28 @@ class Player extends FlxSprite {
 			} else if (_right) {
 				mA -= 45;
 			}
+			facing = FlxObject.DOWN;
 		} else if (_left) {
 			mA = 180;
+			facing = FlxObject.LEFT;
 		} else if (_right) {
 			mA = 0;
+			facing = FlxObject.RIGHT;
 		}
 
 		velocity.set(speed, 0);
 		velocity.rotate(FlxPoint.weak(0, 0), mA);
+
+		// if the player is moving (velocity is not 0 for either axis), we need to change the animation to match their facing
+		if ((velocity.x != 0 || velocity.y != 0) && touching == FlxObject.NONE) {
+			switch (facing) {
+				case FlxObject.LEFT, FlxObject.RIGHT:
+					animation.play("lr");
+				case FlxObject.UP:
+					animation.play("u");
+				case FlxObject.DOWN:
+					animation.play("d");
+			}
+		}
 	}
 }
